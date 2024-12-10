3446a97d679f0652c229a103c09bdf27ec38438c
TalkingPotato90 <star901210@hanmail.net>
2024-12-10 16:11:34 +0900

    fix: 누락 어노테이션 추가

--- backend/src/main/java/kr/co/gitgardner/graphql/UserGraphQL.java
+++ backend/src/main/java/kr/co/gitgardner/graphql/UserGraphQL.java
@@ -1,5 +1,6 @@
 package kr.co.gitgardner.graphql;
 
+import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class UserGraphQL {
 
+    @QueryMapping
     public User user(@AuthenticationPrincipal OAuth2User principal) {
         return new User(
                 principal.getAttribute("id"),
