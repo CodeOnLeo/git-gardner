f54bf08d55afea054c0415d43fe2088d38d29a69
TalkingPotato90 <star901210@hanmail.net>
2024-12-10 16:10:12 +0900

    feat: GraphQL 데이터 처리용 클래스 구현

--- backend/src/main/java/kr/co/gitgardner/graphql/UserGraphQL.java
+++ backend/src/main/java/kr/co/gitgardner/graphql/UserGraphQL.java
@@ -1,7 +1,19 @@
 package kr.co.gitgardner.graphql;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class UserGraphQL {
+
+    public User user(@AuthenticationPrincipal OAuth2User principal) {
+        return new User(
+                principal.getAttribute("id"),
+                principal.getAttribute("name"),
+                principal.getAttribute("login"),
+                principal.getAttribute("avatar_url"),
+                principal.getAttribute("email")
+        );
+    }
 }
