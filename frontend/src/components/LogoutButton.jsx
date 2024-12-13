b66c9594f3c4edf1b0e77edf5ac817f34ce32576
TalkingPotato90 <star901210@hanmail.net>
2024-12-13 12:29:24 +0900

    fix: 버튼 폼 제출 방지 및 디자인 수정

--- frontend/src/components/LogoutButton.jsx
+++ frontend/src/components/LogoutButton.jsx
@@ -1,3 +1,5 @@
+import {Button, LogoutInfo} from "./styles/CommonStyles";
+
 const LogoutButton = () => {
     const handleLogout = () => {
         window.location.href = "http://localhost:8080/logout";
@@ -5,14 +7,15 @@ const LogoutButton = () => {
 
     return (
         <div>
-            <button onClick={handleLogout}>애플리케이션 로그아웃</button>
-            <p>
-                GitHub 계정에서 완전히 로그아웃하려면
+            <Button type="button" onClick={handleLogout}>애플리케이션 로그아웃</Button>
+            <br/>
+            <LogoutInfo>
+                GitHub 계정에서 완전히 로그아웃하려면 <br/>
                 <a href="https://github.com/logout" target="_blank" rel="noopener noreferrer">
                     여기를 클릭
                 </a>
                 하세요.
-            </p>
+            </LogoutInfo>
         </div>
     );
 };
