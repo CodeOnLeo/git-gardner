a178d56b4008b0c8409e649eb2dbf5f1437d7404
TalkingPotato90 <star901210@hanmail.net>
2024-12-13 15:33:10 +0900

    feat: cors 설정 추가

--- backend/src/main/java/kr/co/gitgardner/config/SecurityConfig.java
+++ backend/src/main/java/kr/co/gitgardner/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package kr.co.gitgardner.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -17,11 +19,18 @@ public class SecurityConfig {
         http
                 // TODO: csrf 처리 필요
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
+                .securityContext(securityContext -> securityContext.requireExplicitSave(false))
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/graphiql","graphql").permitAll()
+                        .requestMatchers("/authenticated","/graphiql","graphql").permitAll()
                         .requestMatchers("/").permitAll()
                         .anyRequest().authenticated())
-                .oauth2Login(oauth2 -> oauth2.defaultSuccessUrl("http://localhost:3000/dashboard", true))
+                .oauth2Login(oauth2 -> oauth2
+                        .successHandler((request, response, authentication) -> {
+                            response.sendRedirect("http://localhost:3000/dashboard");
+                        })
+                )
                 .logout(logout -> logout
                         .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                         .logoutSuccessUrl("http://localhost:3000")
---
+++ backend/src/main/java/kr/co/gitgardner/controller/AuthenticationController.java
@@ -0,0 +1,20 @@
+package kr.co.gitgardner.controller;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController {
+
+    @GetMapping("/authenticated")
+    public boolean isAuthenticated(HttpSession session) {
+        if (session != null) {
+            Object securityContext = session.getAttribute("SPRING_SECURITY_CONTEXT");
+            System.out.println(securityContext);
+            return securityContext != null;
+        }
+
+        return false;
+    }
+}
