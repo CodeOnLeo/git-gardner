9bc09f9bae3f485c73877aff3b93b5f2db494975
TalkingPotato90 <star901210@hanmail.net>
2024-12-13 14:17:29 +0900

    feat: 인증정보가 없는 경우 홈화면으로 연결

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,20 @@ import './App.css';
 import LoginPage from "./pages/LoginPage";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import DashBoard from "./pages/DashBoard";
+import {AuthProvider} from "./context/AuthContext";
+import PrivateRoute from "./routes/PrivateRoute";
 
 function App() {
-  return (
-      <BrowserRouter>
-          <Routes>
-              <Route path="/" element={<LoginPage />} />
-              <Route path="/dashboard" element={<DashBoard />} />
-          </Routes>
-      </BrowserRouter>
-  );
+    return (
+        <AuthProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<LoginPage/>}/>
+                    <Route path="/dashboard" element={<PrivateRoute><DashBoard/></PrivateRoute>}/>
+                </Routes>
+            </BrowserRouter>
+        </AuthProvider>
+    );
 }
 
 export default App;
