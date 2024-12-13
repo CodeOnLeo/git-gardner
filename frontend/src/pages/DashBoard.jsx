c257d852f387c3f38d42269ce267e38cdb890539
TalkingPotato90 <star901210@hanmail.net>
2024-12-13 13:26:22 +0900

    feat: 이메일 정보 백엔드로 전송

--- frontend/src/pages/DashBoard.jsx
+++ frontend/src/pages/DashBoard.jsx
@@ -2,6 +2,44 @@ import LogoutButton from "../components/LogoutButton";
 import {Button, Form, Input, StyledWrapper, Title} from "../components/styles/CommonStyles";
 
 const DashBoard = () => {
+    const handleEmailSubmit = () => {
+        const email = document.querySelector('input[name="email"]').value;
+
+        if(!email){
+            alert("이메일 주소를 입력해주세요.")
+            return;
+        }
+
+        fetch("http://localhost:8080/graphql", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                query: `mutation {
+                    registerEmail(email: "${email}") {
+                        success
+                        message
+                    }
+                }
+                `,
+            }),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+            const result = data.data.registerEmail;
+            if(result.success){
+                alert(result.message);
+            }else{
+                alert(result.message);
+            }
+            })
+            .catch((err) => {
+                console.log("[ERROR]" + err);
+                alert("서버 요청에 실패했습니다.");
+            });
+    };
+
     return (
         <StyledWrapper>
             <Form>
@@ -9,7 +47,7 @@ const DashBoard = () => {
                     Git Gardner<span>알림을 받을 이메일을 입력하세요</span>
                 </Title>
                 <Input type="email" placeholder="Email 주소 입력" name="email"/>
-                <Button>
+                <Button type={"button"} onClick={ handleEmailSubmit}>
                     Email 등록하기
                     <svg className="icon" xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24"
                          fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round">
