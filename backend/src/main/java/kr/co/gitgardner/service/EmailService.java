17d760c399adc6c44412d81bc182389c86346f56
TalkingPotato90 <star901210@hanmail.net>
2024-12-18 17:40:24 +0900

    feat: 메일 발송 기능 추가

---
+++ backend/src/main/java/kr/co/gitgardner/service/EmailService.java
@@ -0,0 +1,25 @@
+package kr.co.gitgardner.service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+    private final JavaMailSender mailSender;
+
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendMail(String to, String subject, String text) throws MessagingException {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+        mimeMessageHelper.setTo(to);
+        mimeMessageHelper.setSubject(subject);
+        mimeMessageHelper.setText(text, true);
+        mailSender.send(mimeMessage);
+    }
+}
